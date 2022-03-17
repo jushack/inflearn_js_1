43e57f1006b36e0c365d1e19272ce9320fd6a266
jushack <3.jushack.1@gmail.com>
2022-03-17 14:19:05 +0900

    if_1

---
+++ ch08/if_1.js
@@ -0,0 +1,8 @@
+var userID = window.prompt("사용자 아이디 입력하세요");
+
+        if(userID == "Jang") {
+            alert(userID + "님 환영합니다.");
+        }
+        else {
+            alert("아이드를 확인해주세요.");
+        }
